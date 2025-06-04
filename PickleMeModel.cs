390ba6918bbcd97e5425905846c73b808c65a4ae
Jordan Woodruff <boogermanus@gmail.com>
2025-06-04 16:25:26 -0500

    move model

---
+++ PickleMeModel.cs
@@ -0,0 +1,18 @@
+﻿namespace PickleMe;
+
+public class PickleMeModel
+{
+    public double Cups { get; set; }
+    public double Water { get; private set; }
+    public double Vinegar { get; private set; }
+    public double Sugar { get; private set; }
+    public double Salt { get; private set; }
+
+    public void Calculate()
+    {
+        Water = Cups / 2;
+        Vinegar = Cups / 2;
+        Sugar = Cups / 4;
+        Salt = Cups * 16 / 16;
+    }
+}
