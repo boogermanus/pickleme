d960a9ddd83bd13871fb0fd4167b886b3e7501bf
Jordan Woodruff <boogermanus@gmail.com>
2025-06-15 19:54:20 -0500

    cleanup, adding store, and blazor sln

---
+++ PickleMe.Angular/src/Stores/PickleMeStore.ts
@@ -0,0 +1,20 @@
+﻿import {signalStore, withComputed, withState} from '@ngrx/signals';
+import {computed} from '@angular/core';
+
+type PickleMeState = {
+  cups: number;
+}
+
+const initialState: PickleMeState = {
+  cups: 0,
+}
+
+export const PickleMeStore = signalStore(
+  withState<PickleMeState>(initialState),
+  withComputed((store) => ({
+    water: computed(() => store.cups() / 2),
+    vinegar: computed(() => store.cups() / 2),
+    sugar: computed(() => store.cups() / 4),
+    salt: computed(() => store.cups() * 16 / 16)
+  }))
+)
